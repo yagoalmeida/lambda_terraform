7f50472c6fd8ff6ebaa718487b40d840944cb2c8
Yago Almeida <33463038+yagoalmeida@users.noreply.github.com>
2022-05-17 00:36:40 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "aws_lambda_function" "test_lambda" {
 
   function_name = "poc_lambda_iac"
   role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "lambda_function.lambda_handler"
+  handler       = "lambda_handler"
 
   source_code_hash = data.archive_file.python_lambda_package.output_base64sha256
   runtime          = "python3.8"
